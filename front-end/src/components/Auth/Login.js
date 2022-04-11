3101ed8e3afcb1032d7fd54129829daf98ea4620
Isaque Alves <isaquea785@gmail.com>
2022-04-11 12:33:17 -0300

    :sparkles: Cria verificacoes de campos com Formik e yup

--- front-end/src/components/Auth/Login.js
+++ front-end/src/components/Auth/Login.js
@@ -1,5 +1,8 @@
+import React from "react";
 import { makeStyles } from "@material-ui/styles";
 import { Grid, Typography, TextField, Paper, Button } from "@mui/material";
+import { useFormik } from "formik";
+import * as yup from "yup";
 import logo from "../../assets/logo-colorida.png";
 
 import Footer from "../Utils/Footer";
@@ -28,8 +31,34 @@ const useStyles = makeStyles({
   },
 });
 
-export default function Login(props) {
+const Login = (props) => {
   const classes = useStyles(props);
+  // const handleSubmit = (values) => {
+  //   console.log(values);
+  // };
+
+  const validationLogin = yup.object({
+    email: yup
+      .string("Enter your email")
+      .email("Enter a valid email")
+      .required("Email is required"),
+    password: yup
+      .string("Enter your password")
+      .min(6, "Password should be of minimum 6 characters length")
+      .required("Password is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: validationLogin,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <Grid
       container
@@ -37,37 +66,59 @@ export default function Login(props) {
       alignItems="center"
       className={classes.root}
     >
-      <Grid item md="3">
+      <Grid item md={3}>
         <img src={logo} />
       </Grid>
-      <Grid item md="7">
+      <Grid item md={7}>
         <Paper variant="outlined" className={classes.paper}>
           <Grid container direction="column" alignItems="flex-start">
             <Typography className={classes.title}>Login</Typography>
-            <TextField
-              className={classes.field}
-              id="filled-email-input"
-              fullWidth
-              label="Email"
-              type="email"
-              variant="outlined"
-            />
-            <TextField
-              className={classes.field}
-              id="filled-password-input"
-              fullWidth
-              label="Password"
-              type="password"
-              autoComplete="current-password"
-              variant="outlined"
-            />
-            <Button className={classes.button} variant="contained">
-              Login
-            </Button>
+            <form onSubmit={formik.handleSubmit}>
+              <TextField
+                className={classes.field}
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+                id="filled-email-input"
+                name="email"
+                fullWidth
+                label="Email"
+                type="email"
+                variant="outlined"
+              />
+
+              <TextField
+                className={classes.field}
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }
+                helperText={formik.touched.password && formik.errors.password}
+                id="filled-password-input"
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                variant="outlined"
+              />
+
+              <Button
+                className={classes.button}
+                variant="contained"
+                type="submit"
+              >
+                Login
+              </Button>
+            </form>
           </Grid>
         </Paper>
         <Footer />
       </Grid>
     </Grid>
   );
-}
+};
+
+export default Login;
