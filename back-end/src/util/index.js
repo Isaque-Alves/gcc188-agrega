5f6907a1ed0ae98eee28165eceabed09beb50d12
iagoq <18238046+iagocq@users.noreply.github.com>
2022-04-13 10:41:03 -0300

    Adiciona CRUD de links no backend

--- back-end/src/util/index.js
+++ back-end/src/util/index.js
@@ -2,9 +2,7 @@ import bcrypt from 'bcrypt';
 
 let Util = {
     __necessario(req, res, campos, requeridos) {
-        const r = { valido: true };
-        const t = () => true;
-        const v = Util.campoInvalido;
+        const r = { valido: true, c: {} };
         for (const nome of requeridos) {
             let attr = campos[nome];
             let c = req.body[nome] || req.params[nome];
@@ -26,13 +24,61 @@ let Util = {
                 (attr.invalido || v)(res, nome);
                 break;
             }
-            r[nome] = c;
+            r.c[nome] = c;
         }
         return r;
     },
 
     generateNecessario(campos) {
         return (req, res, ...requeridos) => Util.__necessario(req, res, campos, requeridos);
+    },
+
+    requer(...c) {
+        async function handler(req, res, next) {
+            let valido = true;
+            const t = () => true;
+            const v = Util.campoInvalido;
+
+            if (!req.r) {
+                req.r = {};
+            }
+
+            for (const nome of c) {
+                if (req.r[nome] != undefined) continue;
+
+                let info = campos[nome];
+                let campo = req.body[nome] || req.params[nome];
+
+                if (req.user) {
+                    campo = campo || req.user[nome];
+                }
+                if (req.admin) {
+                    campo = campo || req.admin[nome];
+                }
+
+                if (campo == undefined) {
+                    valido = false;
+                    Util.campoFaltando(res, nome);
+                    break;
+                }
+                if (!(info.validar || t)(campo)) {
+                    valido = false;
+                    (info.invalido || v)(res, nome);
+                    break;
+                }
+                req.r[nome] = campo;
+            }
+
+            if (valido) {
+                next();
+            }
+        }
+
+        return handler;
+    },
+
+    async requerParam(req, res, next) {
+        await Util.requer(...Object.keys(req.params))(req, res, next);
     },
 
     async processarSenha(s) {
@@ -86,7 +132,30 @@ let Util = {
         if (!senha) return false;
         const tamanho = [...senha].length;
         return tamanho >= 6 && tamanho <= 50;
+    },
+
+    validarUrl(url) {
+        let u;
+        try {
+            u = new URL(url);
+        } catch (_) {
+            return false;
+        }
+
+        return u.protocol === 'http:' || u.protocol === 'https:';
     }
 }
+
+const campos = {
+    nome: { validar: Util.validarNome },
+    senha: { validar: Util.validarSenha, invalido: Util.senhaInvalida },
+    senhaAntiga: { validar: Util.validarSenha, invalido: Util.senhaInvalida },
+    email: { validar: Util.validarEmail },
+    url: { validar: Util.validarUrl },
+    id: { },
+    gid: { },
+    lid: { },
+    cid: { }
+};
 
 export { Util };
