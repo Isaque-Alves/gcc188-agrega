fe837e4ada398f78d8aede7e20a649c95fbd85c6
Luis Fernando <luisfsf1313@gmail.com>
2022-04-12 21:49:15 -0300

    Alterações

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+
+import App from "./App";
+import Login from "./views/Login";
+import Register from "./views/Register";
+import Grupos from "./views/Grupos";
+import Grupos2 from "./views/Grupos2";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="login" element={<Login />} />
+      <Route path="registrar" element={<Register />} />
+      <Route path="grupos" element={<Grupos />} />
+      <Route path="grupos2" element={<Grupos2 />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
