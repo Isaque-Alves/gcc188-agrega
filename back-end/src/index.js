38c1959387b6886ecf84d7964de32d88328b2fc5
iagoq <18238046+iagocq@users.noreply.github.com>
2022-04-13 17:20:36 -0300

    Adiciona configuração de CORS

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -2,14 +2,17 @@ import { db } from '~/db';
 import express from 'express';
 import router from '~/routes';
 import cookieParser from 'cookie-parser';
+import c from '~/config';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 app.use(router);
 
 (async () => {
     await db.sync({ logging: console.log });
-    app.listen(3000);
+    app.listen(c.BACKEND_PORT);
 })();
