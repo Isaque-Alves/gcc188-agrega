29f240658840d1859297f332bba8ca29a65e946b
iagoq <18238046+iagocq@users.noreply.github.com>
2022-04-13 00:22:37 -0300

    Adiciona lógica de autenticação

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,10 +1,12 @@
 import { db } from '~/db';
 import express from 'express';
 import router from '~/routes';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(router);
 
 (async () => {
