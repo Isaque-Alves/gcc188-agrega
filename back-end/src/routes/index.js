2b345b2be8f4c2b0b3a331766ca0ef68809d5ce3
iagoq <18238046+iagocq@users.noreply.github.com>
2022-04-13 01:24:28 -0300

    Adiciona CRUD de grupos de link

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,22 +1,34 @@
 import { UsuarioController as UC } from '~/controllers/usuario';
+import { GrupoLinkController as GC } from '~/controllers/grupos';
 import express from 'express';
 
 const router = express.Router();
-const uRouter = express.Router({ mergeParams: true });
+const usrRouter = express.Router();
 const admRouter = express.Router();
+
+const usrGrpRouter = express.Router({mergeParams: true});
 
 router.post('/login', UC.login);
 router.post('/registrar', UC.registrar);
 
-router.use('/usuario', UC.usuario, uRouter);
-uRouter.get('/', UC.encontrar);
-uRouter.put('/', UC.atualizar);
-uRouter.put('/senha', UC.atualizarSenha);
+router.get('/usuario/:id/grupos', GC.listar);
+router.get('/usuario/:id/grupo/:gid', GC.encontrar)
+
+router.use('/usuario', UC.usuario, usrRouter);
+usrRouter.get('/', UC.encontrar);
+usrRouter.put('/', UC.atualizar);
+usrRouter.put('/senha', UC.atualizarSenha);
+usrRouter.get('/grupos', GC.listar);
+usrRouter.post('/grupo', GC.criar);
+usrRouter.use('/grupo/:gid', GC.grupoProprio, usrGrpRouter);
+usrGrpRouter.get('/', GC.encontrar);
+usrGrpRouter.put('/', GC.atualizar);
+usrGrpRouter.delete('/', GC.apagar);
 
 router.use('/admin', UC.admin, admRouter);
 // admRouter.post('/login', UC.adminLogin);
-admRouter.get('/usuarios', UC.todos);
-admRouter.get('/usuarios/:id', UC.encontrar);
-admRouter.delete('/usuarios/:id', UC.deletar);
+admRouter.get('/usuarios', UC.listar);
+admRouter.get('/usuario/:id', UC.encontrar);
+admRouter.delete('/usuario/:id', UC.apagar);
 
 export default router;
